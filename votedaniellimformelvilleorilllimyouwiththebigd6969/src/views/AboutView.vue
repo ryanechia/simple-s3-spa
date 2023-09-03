4e2bee695684ce83939a11512d51ed0099ff254b
Ryane Chia <ryanechia@outlook.com>
2023-09-04 02:25:39 +1000

    content update for about page

--- votedaniellimformelvilleorilllimyouwiththebigd6969/src/views/AboutView.vue
+++ votedaniellimformelvilleorilllimyouwiththebigd6969/src/views/AboutView.vue
@@ -1,22 +1,42 @@
 <template>
   <div class="page-container">
     <div class="textBlock">
-      <h1>Background</h1>
+      <h1>Get to know me!</h1>
       <div>
-        <p>Graduated from UWA, CA Accredited, Lover of BBT, Drives at the speed limit.</p>
+        <p>
+          My first priority as your Councillor, will be addressing future rate rises for all residents. I understand the
+          importance of fostering a culture of accountability, to best steward your public resources. I believe I am
+          uniquely positioned to challenge and guide the City’s resource allocation approach, by leveraging my
+          experience working in international consultancy firms, government, technology start-ups, and ASX-listed
+          companies.
+        </p>
+        <p>
+          I believe that a variety of factors go into what makes our City a great place to live - affordability,
+          livability, our natural and built environment, the social networks, and economic opportunities. Therefore,
+          other key areas that I will focus on will include - improving community safety, encouraging the growth of our
+          urban tree canopy, sustainable strategies to tackle road safety, creating inclusive spaces within the City,
+          and establishing multi-use spaces to build valuable shared experiences for everyone who calls the City of
+          Melville home.
+        </p>
+        <p>
+          I am running to be your local Councillor because I am passionate about our community. Our ward, and the
+          greater City of Melville, is an amazing place. I know I’m not the only one that values all that our community
+          has to offer - our world-famous jacaranda trees, being able to safely cycle along the beautiful Swan River
+          across the Canning Bridge into the CBD, and enjoying the local produce on offer at the many small businesses
+          dotted throughout the City. These often overlooked everyday experiences are what I want to champion and
+          protect for all current and future residents.
+        </p>
       </div>
     </div>
     <div class="textBlock">
       <h1>Professional Experience</h1>
       <div>
-        <p>I worked at a local council before, and my auditor senses begun tingling.</p>
-        <p> There is no way that the rates can increase by 9% in this year alone</p>
-      </div>
-    </div>
-    <div class="textBlock">
-      <h1>Interests</h1>
-      <div>
-        <p>A good deal on OzBargain</p>
+        <p>
+          I have key skills in external financial audit for ASX 200 companies, producing comprehensive asset
+          management plans, financial management systems, and business analysis and advice. My specialised technical
+          abilities as a Chartered Accountant will allow me to critically scrutinise key areas of the Council’s
+          financial management, such as the Long term Financial Plan and the Asset Management Plan.
+        </p>
       </div>
     </div>
   </div>
